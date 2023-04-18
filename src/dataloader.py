3d2f648b1541204fda8653e1d687e1e82b211bd6
Pratyush <pratyushiitd>
2023-04-18 16:11:19 +0530

    gpt2 with eval complete

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,23 +1,51 @@
 from imports import *
 
-class DataLoader():
-    def __init__(self, data_path):
-        data = []
-        with jsonlines.open(data_path) as reader:
-            for obj in reader:
-                data.append(self.get_input(obj))
-        self.data = data
+class TODDataset(Dataset):
+    def __init__(self, data_paths, tokenizer, aux_input=False, val=False):
+        self.tokenizer = tokenizer
+        self.input_ids = []
+        self.attention_mask = []
+        for data_path in data_paths:
+            print('Loading data from {}'.format(data_path))
+            with jsonlines.open(data_path) as reader:
+                for obj in reader:
+                    input_raw = self.get_input(obj) if not aux_input else self.get_aux_input(obj)
+                    output_raw = self.get_output(obj) if not val else ''
+                    text = "<bos> " + input_raw.strip() + " <sep> " + output_raw.strip() + " <eos>"
+                    encoded_input = tokenizer(text, return_tensors='pt', return_attention_mask=True)
+                    self.input_ids.append(encoded_input['input_ids'][0])
+                    self.attention_mask.append(encoded_input['attention_mask'][0])
+        print("Number of examples: {}".format(len(self.input_ids))) 
     def __len__(self):
-        return len(self.data)
+
+        return len(self.input_ids)
     
     def __getitem__(self, idx):
-        return self.data[idx]
-
+        return {"input_ids": self.input_ids[idx], "attention_mask": self.attention_mask[idx]}
+    
     def get_input(self, obj):
+        input_str = obj['input']
+        return input_str
+
+    def get_aux_input(self, obj):
         input_str = obj['input']
         history_str = ' '.join([x['user_query'] + ' ' + x['response_text'] for x in obj['history']])
         user_lists_str = ' '.join([l['name'] + ' ' + ' '.join(l['items']) for l in obj['user_lists']])
         user_notes_str = ' '.join([n['name'] + ' ' + n['content'] for n in obj['user_notes']])
         user_contacts_str = ' '.join(obj['user_contacts'])
-        # return input_str + ' ' + history_str + ' ' + user_lists_str + ' ' + user_notes_str + ' ' + user_contacts_str
-        return obj['output']
+        # return input_str + ' <history> ' + history_str.strip() + ' <user_list> ' + user_lists_str.strip() + ' <user_notes> ' + user_notes_str.strip() + ' <user_contacts> ' + user_contacts_str.strip()
+        return input_str + ' <history> ' + history_str.strip()
+    def get_output(self, obj):
+        output_str = obj['output']
+        return output_str
+    
+def collate_fn(batch):
+    return
+    # Group samples by length
+    batch = sorted(batch, key=lambda x: x['input_ids'].size(0))
+    
+    # Get input_ids and attention_mask tensors
+    input_ids = torch.nn.utils.rnn.pad_sequence([x['input_ids'] for x in batch], batch_first=True, padding_value=tokenizer.pad_token_id)
+    attention_mask = torch.nn.utils.rnn.pad_sequence([x['attention_mask'] for x in batch], batch_first=True, padding_value=0)
+    
+    return {"input_ids": input_ids, "attention_mask": attention_mask}
--- src/imports.py
+++ src/imports.py
@@ -1,4 +1,11 @@
-import jsonlines, argparse
+import jsonlines, argparse, random, os, sys
 from collections import defaultdict
 import torch
-from transformers import GPT2Tokenizer, GPT2Model
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import random_split
+import numpy as np
+from tqdm import tqdm
+from transformers import TrainingArguments
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config, set_seed
+from transformers import AdamW, get_linear_schedule_with_warmup
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
