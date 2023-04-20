9de8f70d87e8821de8bbae4b1f8462e1b6a5725c
Pratyush <pratyushiitd>
2023-04-20 21:50:01 +0530

    added test script

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 
 from imports import *
 
-from dataloader import TODDataset
 from utils import *
 from evaluations import Evaluator
 from gpt2 import GPT2_TOD
-# from t5 import T5_TOD
+from t5 import T5_TOD
 
 if __name__ == '__main__':
     seed_everything(42)
@@ -17,9 +16,22 @@ if __name__ == '__main__':
 
         if (args['model'].startswith('gpt2')):
             model = GPT2_TOD(args)
-
+        elif (args['model'].startswith('t5')):
+            model = T5_TOD(args)
         model.train(args)
 
+        with open(args['model_path'], 'wb') as fp:
+            pickle.dump(model, fp)
+
     if (args['mode'] == 'test'):
-        pass
+
+
+        try:
+            with open(args['model_path'], 'rb') as fp:
+                model = pickle.load(fp)
+        except:
+            print("Model loading failed")
+            sys.exit(0)
+
+        model.predict(args)
         
